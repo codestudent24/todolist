a8a28ff39448f7cdd68f8bc9e4195a0309ea3871
codestudent24 <belgorod2407@yandex.ru>
2023-02-09 22:38:24 +1000

    feat: add functions to delete and change todo items

--- app.js
+++ app.js
@@ -1,60 +1,190 @@
-// Globals
-const todoList = document.getElementById("todo-list");
-let todos = [];
-let users = [];
+(function () {
+  // Globals
+  const todoList = document.getElementById("todo-list");
+  const userSelect = document.getElementById("user-todo");
+  const form = document.querySelector("form");
+  let todos = [];
+  let users = [];
 
-// Attach events
-document.addEventListener("DOMContentLoaded", initApp);
+  // Attach events
+  document.addEventListener("DOMContentLoaded", initApp);
+  form.addEventListener("submit", handleSubmit);
 
-// Basic logic
-function getUserName(userId) {
-  const user = users.find((u) => u.id === userId);
-  return user.name;
-}
+  // Basic logic
+  function getUserName(userId) {
+    const user = users.find((u) => u.id === userId);
+    return user.name;
+  }
 
-function printTodo({ id, userId, title, completed }) {
-  const li = document.createElement("li");
-  li.className = "todo-item";
-  li.dataset.id = id;
-  li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
-    userId
-  )}</b></span>`;
+  function printTodo({ id, userId, title, completed }) {
+    const li = document.createElement("li");
+    li.className = "todo-item";
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+      userId
+    )}</b></span>`;
 
-  const status = document.createElement("input");
-  status.type = "checkbox";
-  status.checked = completed;
+    const status = document.createElement("input");
+    status.type = "checkbox";
+    status.checked = completed;
+    status.addEventListener("change", handleTodoChange);
 
-  const close = document.createElement("span");
-  close.innerHTML = "&times;";
-  close.className = "close";
+    const close = document.createElement("span");
+    close.innerHTML = "&times;";
+    close.className = "close";
+    close.addEventListener("click", handleClose);
 
-  li.prepend(status);
-  li.append(close);
-  todoList.prepend(li);
-}
+    li.prepend(status);
+    li.append(close);
+    todoList.prepend(li);
+  }
 
-// Event logic
-function initApp() {
-  Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
-    [todos, users] = values;
+  function createUserOption(user) {
+    const option = document.createElement("option");
+    option.value = user.id;
+    option.innerText = user.name;
 
-    // Push to html
-    todos.forEach((todo) => printTodo(todo));
-  });
-}
+    userSelect.append(option);
+  }
 
-// Async logic
+  function removeTodo(todoId) {
+    todos = todos.filter((todo) => todo.id !== todoId);
+    console.log("todoId = " + todoId);
 
-async function getAllTodos() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const data = await response.json();
+    const todo = todoList.querySelector(`[data-id="${todoId}"]`);
+    console.log(todo);
+    todo.querySelector("input").removeEventListener("change", handleTodoChange);
+    todo.querySelector(".close").removeEventListener("click", handleClose);
 
-  return data;
-}
+    todo.remove();
+  }
 
-async function getAllUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
+  function alertError(error) {
+    alert(error.message);
+  }
 
-  return data;
-}
+  // Event logic
+  function initApp() {
+    Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+      [todos, users] = values;
+
+      // Push to html
+      todos.forEach((todo) => printTodo(todo));
+      users.forEach((user) => createUserOption(user));
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    createTodo({
+      userId: Number(form.user.value),
+      title: form.todo.value,
+      completed: false,
+    });
+    console.log(form.todo.value);
+    console.log(form.user.value);
+  }
+
+  function handleTodoChange() {
+    const todoId = this.parentElement.dataset.id;
+    const completed = this.checked;
+
+    toggleTodoComplete(todoId, completed);
+  }
+
+  function handleClose() {
+    const todoId = this.parentElement.dataset.id;
+    deleteTodo(todoId);
+  }
+  // Async logic
+
+  async function getAllTodos() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=15"
+      );
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      alertError(error);
+    }
+  }
+
+  async function getAllUsers() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      alertError(error);
+    }
+  }
+
+  async function createTodo(todo) {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos",
+        {
+          method: "POST",
+          body: JSON.stringify(todo),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const newTodo = await response.json();
+      printTodo(newTodo);
+    } catch (error) {
+      alertError(error);
+    }
+  }
+
+  async function toggleTodoComplete(todoId, completed) {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify({ completed }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to connect with server! Please, try later.");
+      }
+    } catch (error) {
+      alertError(error);
+    }
+  }
+
+  async function deleteTodo(todoId) {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        //removeTask
+        removeTodo(todoId);
+      } else {
+        throw new Error("Failed to connect with server! Please, try later.");
+      }
+    } catch (error) {
+      alertError(error);
+    }
+  }
+})();
